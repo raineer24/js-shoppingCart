c8477c84075f2ad5051eb5e09a707d01e5a8a7ae
Raineer <delaritaraineer81@gmail.com>
2019-04-06 22:15:50 +0800

    this.removeItem(id) cartContent

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ class UI {
     cartContent.addEventListener('click', event => {
       if (event.target.classList.contains('remove-item')) {
         let removeItem = event.target;
-        console.log(removeItem);
+        let id = removeItem.dataset.id;
 
       }
     });
