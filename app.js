362e190a3cdd00c38721ae90b4b8278054e5f53f
Raineer <delaritaraineer81@gmail.com>
2019-04-06 10:55:45 +0800

    product map function

--- app.js
+++ app.js
@@ -21,9 +21,23 @@ class Products {
       let data = await result.json();
       let products = data.items;
       products = products.map(item => {
-
+        const {
+          title,
+          price
+        } = item.fields;
+        const {
+          id
+        } = item.sys;
+        const
+          image = item.fields.image.fields.file.url;
+        return {
+          title,
+          price,
+          id,
+          image
+        };
       });
-      return data;
+      return products;
     } catch (error) {
       console.log(error);
     }
