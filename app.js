7a19435918cfbaf7b2d0a85951119b5984525b95
Raineer <delaritaraineer81@gmail.com>
2019-04-06 10:57:04 +0800

    change rename data to product

--- app.js
+++ app.js
@@ -55,5 +55,5 @@ document.addEventListener("DOMContentLoaded", () => {
   const products = new Products();
 
   // get all products
-  products.getProducts().then(data => console.log(data));
+  products.getProducts().then(products => console.log(products));
 });
