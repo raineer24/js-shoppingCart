91d4cffb032bfc420bfd2a8ffeeb694087bdb943
Raineer <delaritaraineer81@gmail.com>
2019-04-06 22:32:20 +0800

    event target addamount dataset id chevron up

--- app.js
+++ app.js
@@ -163,7 +163,9 @@ class UI {
         cartContent.removeChild(removeItem.parentElement.parentElement);
         this.removeItem(id);
       } else if (event.target.classList.contains("fa-chevron-up")) {
-
+        let addAmount = event.target;
+        let id = addAmount.dataset.id;
+        console.log(addAmount);
       }
     });
   }
